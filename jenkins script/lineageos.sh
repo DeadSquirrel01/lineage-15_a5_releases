256a9275a16cfb234e923a9cdcb689f474dae50f
DeadSquirrel01 <m.marcaccini01@gmail.com>
2018-01-04 10:53:02 +0000

    Lineage 15.0 -> 15.1 and notify the user to download a camera app from play store to get it working

--- jenkins script/lineageos.sh
+++ jenkins script/lineageos.sh
@@ -5,10 +5,10 @@ export TMPDIR="$WORKSPACE/tmp"
 rm -rf $TMPDIR
 mkdir $TMPDIR
 # repopick needs "repo" command
-REPO="$WORKSPACE/lineageos-15.0/.repo/repo"
+REPO="$WORKSPACE/lineageos-15.1/.repo/repo"
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$REPO:$PATH
 FBASE_PATCH="0001-fw-base-Enable-home-button-wake.patch" # patch to wake device with home button
-cd lineageos-15.0
+cd lineageos-15.1
 repo sync --force-sync
 # Some needed commits haven't been pushed to lineage repos, yet. Let's repopick them, then
 # Script can be found here http://msm8916.com/~vincent/repopicks.sh
@@ -29,16 +29,26 @@ export USE_CCACHE=1
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 export ANDROID_JACK_VM_ARGS="$JACK_SERVER_VM_ARGUMENTS"
 # lineage 15 zip path
-export ROM_PATH="$WORKSPACE/lineageos-15.0/out/target/product/a5ultexx/lineage-15.0-*-UNOFFICIAL-a5ultexx.zip"
+export ROM_PATH="$WORKSPACE/lineageos-15.1/out/target/product/a5ultexx/lineage-15.1-*-UNOFFICIAL-a5ultexx.zip"
 # Kill jack server if active and start it (still for low ram reasons)
 ./prebuilts/sdk/tools/jack-admin kill-server || true
 ./prebuilts/sdk/tools/jack-admin start-server || true
 # Start building
 source build/envsetup.sh
 brunch a5ultexx
+
+# Tell the user to download a camera app that supports legacy ones to use it, because in 8.1 camera does not support legacy cameras.
+# Do it by writing it in updater-scipt
+mkdir $TMPDIR/zip # Gives error if the dir is already present, because zip dir needs to be clean, to not include shit in the zip
+cd $TMPDIR/zip
+cp $ROM_PATH $TMPDIR/los15.zip
+unzip ../los15.zip
+sed -i '/package_extract_file("boot.img", "\/dev\/block\/bootdevice\/by-name\/boot");/a ui_print("SINCE IN 8.1 LEGACY CODE IS FULLY REMOVED, THE ROM DOES NOT PROVIDE CAMERA APP");\nui_print("BECAUSE IT WOULD NOT WORK AT ALL");\nui_print("SO, DOWNLOAD A CAMERA APP FROM PLAY STORE e.g. OPEN CAMERA TO USE CAMERA");' META-INF/com/google/android/updater-script
+zip -r lineage-15.1-$(date +%Y%m%d)-UNOFFICIAL-a5ultexx.zip *
+
 # Create a file containing the sha256sum of the zip.
 # We create it outside the project directory because twrp job will add his twrp sha256sums
-(cd out/target/product/a5ultexx/ && sha256sum lineage-15.0-*-UNOFFICIAL-a5ultexx.zip) > ../../sha256sums_$(date +%Y%m%d).txt
+sha256sum lineage-15.1-*-UNOFFICIAL-a5ultexx.zip > ../../sha256sums_$(date +%Y%m%d).txt
 
 # Publish to github
 export GITHUB_TOKEN=# Secret :P
@@ -48,10 +58,10 @@ export GITHUB_REPO=lineage-15_a5_releases
 # Publish the release
 # Needs to have github-release installed
 echo "Create a new release in https://github.com/DeadSquirrel01/lineage-15_a5_releases"
-github-release release --user $GITHUB_USER --repo $GITHUB_REPO --tag $(date +%Y%m%d) --name "lineageos 15.0 $(date +%Y%m%d)"
+github-release release --user $GITHUB_USER --repo $GITHUB_REPO --tag $(date +%Y%m%d) --name "lineageos 15.1 $(date +%Y%m%d)"
 
 echo "Uploading the lineage 15 zip into github release"
-github-release upload --user $GITHUB_USER --repo $GITHUB_REPO --tag $(date +%Y%m%d) --name "LineageOS 15 $(date +%Y%m%d) SM-A500FU" --file $ROM_PATH
+github-release upload --user $GITHUB_USER --repo $GITHUB_REPO --tag $(date +%Y%m%d) --name "LineageOS 15.1 $(date +%Y%m%d) SM-A500FU" --file lineage-15.1-*-UNOFFICIAL-a5ultexx.zip
 
 cd lineage-15_a5_releases
 git add changelog.txt
